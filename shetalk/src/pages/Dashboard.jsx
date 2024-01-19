096c4727bd0b2d0cb8e81984ba1d0f221fea4abb
anna <nanamint623@gmail.com>
2024-01-19 16:50:54 +0700

    [create] PostCard component, and displaying it on the dashboard pages

--- shetalk/src/pages/Dashboard.jsx
+++ shetalk/src/pages/Dashboard.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from '../components/molecules/Navbar';
 import SideBar from '../components/molecules/Sidebar';
+import { dummyAvatar } from '../assets';
+import PostCard from '../components/molecules/PostCard';
+
+
 
 const Dashboard = () => {
   return (
@@ -14,7 +18,27 @@ const Dashboard = () => {
             </div>
           </div>
 
-          <div className="col-9 mt-5 ">{/* tempat Postingan */}</div>
+          <div className="col-9 mt-5 ">
+            {/* tempat Postingan */}
+
+            <PostCard img={dummyAvatar} 
+            nama="anonim" 
+            tanggal="12/01/2024" 
+            judul="Berapa lama menstruasi yang normal?" 
+            topik="menstruasi"
+         />
+
+
+            <PostCard
+              img={dummyAvatar}
+              nama="anonim"
+              tanggal="12/01/2024"
+              judul="Consent and Coercion: Examining Unwanted Sex Among Married Young Women in India"
+              konten="K.G Santhya bersama empat orang rekannya pernah melakukan sebuah penelitian yang diberi judul “Consent and Coercion: Examining Unwanted Sex Among Married Young Women in India”. Penelitian tersebut menggunakan metode survey dan wawancara mendalam yang melibatkan 1.664 wanita muda yang selengkapnya."
+              topik="menstruasi"
+            />
+
+          </div>
         </div>
       </div>
     </>
