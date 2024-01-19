f6934fb8977f22441fa43274be969a10d76d48db
anna <nanamint623@gmail.com>
2024-01-19 11:41:23 +0700

    [create] Navbar component and Structure folder

---
+++ shetalk/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/molecules/Navbar';
+
+const Dashboard = () => {
+  return (
+    <div>
+ <Navbar />
+    </div>
+  );
+};
+
+export default Dashboard;
