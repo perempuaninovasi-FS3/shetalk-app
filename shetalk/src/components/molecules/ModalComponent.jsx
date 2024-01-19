6c6d0b08a4b9664241bcd448011cf3ee66aa2e7e
anna <nanamint623@gmail.com>
2024-01-19 16:50:54 +0700

    [create] PostCard component, and displaying it on the dashboard pages

--- shetalk/src/components/molecules/ModalComponent.jsx
+++ shetalk/src/components/molecules/ModalComponent.jsx
@@ -3,25 +3,34 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import SelectTopics from './SelectTopik';
 
-
 const ModalComponent = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+
+  
   return (
     <>
-      <Button  onClick={handleShow}  style={{ backgroundColor: '#43d7c2', border: 'none' , marginBottom: '14px', }}>
-       Mulai Diskusi Baru
+      <Button
+        onClick={handleShow}
+        style={{
+          backgroundColor: '#43d7c2',
+          border: 'none',
+          marginBottom: '14px',
+     
+        }}
+      >
+        Mulai Diskusi Baru
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-      <SelectTopics />
+          <SelectTopics />
         </Modal.Header>
         <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-   
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
@@ -33,6 +42,6 @@ const ModalComponent = () => {
       </Modal>
     </>
   );
-}
+};
 
 export default ModalComponent;
