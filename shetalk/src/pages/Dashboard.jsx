c2e3f25f6a89b39a954f4e1ee1bb5f1cb3f2226e
anna <nanamint623@gmail.com>
2024-01-20 09:44:47 +0700

    [Add comment feature]: Implemented functionality to allow users to leave comments on posts. This includes creating CommentInput and Comment components, as well as updating the PostCard component to toggle and display comments

--- shetalk/src/pages/Dashboard.jsx
+++ shetalk/src/pages/Dashboard.jsx
@@ -4,40 +4,36 @@ import SideBar from '../components/molecules/Sidebar';
 import { dummyAvatar } from '../assets';
 import PostCard from '../components/molecules/PostCard';
 
-
-
 const Dashboard = () => {
   return (
     <>
       <Navbar />
+
       <div className="mx-5 ">
         <div className="row mx-5">
+          {/* sidebar */}
           <div className="col-3 ">
             <div className="position-fixed">
               <SideBar />
             </div>
           </div>
 
+          {/* tempat Postingan */}
           <div className="col-9 mt-5 ">
-            {/* tempat Postingan */}
-
-            <PostCard img={dummyAvatar} 
+            <PostCard avatar={dummyAvatar} 
             nama="anonim" 
             tanggal="12/01/2024" 
             judul="Berapa lama menstruasi yang normal?" 
-            topik="menstruasi"
-         />
-
+            topik="menstruasi" />
 
             <PostCard
-              img={dummyAvatar}
+              avatar={dummyAvatar}
               nama="anonim"
               tanggal="12/01/2024"
               judul="Consent and Coercion: Examining Unwanted Sex Among Married Young Women in India"
               konten="K.G Santhya bersama empat orang rekannya pernah melakukan sebuah penelitian yang diberi judul “Consent and Coercion: Examining Unwanted Sex Among Married Young Women in India”. Penelitian tersebut menggunakan metode survey dan wawancara mendalam yang melibatkan 1.664 wanita muda yang selengkapnya."
               topik="menstruasi"
             />
-
           </div>
         </div>
       </div>
