560a9a4e335f83bb6b20c8ed7e24939aed54a7f3
doobeedoobeedam <kwecitra@gmail.com>
2024-01-23 01:35:37 +0700

    make feature topics for posts

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const authRouter = require("../routes/authRouter");
 const userRouter = require("../routes/userRouter");
 const avatarRouter = require("../routes/avatarRouter");
+const topicRouter = require("../routes/topicRouter");
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
+router.use("/topics", topicRouter);
 router.use(avatarRouter);
 router.get("/", function (req, res) {
   return res.status(200).json({ success: true, message: "API ready to use." });
--- routes/topicRouter.js
+++ routes/topicRouter.js
@@ -2,3 +2,7 @@ const avatarController = require("../controllers/Avatars/avatarController");
 const express = require("express");
 const router = express.Router();
 const verifyApiKey = require("../middlewares/verifyApiKey");
+const topicsController = require("../controllers/Topics/topicsController");
+router.get("/", verifyApiKey, topicsController.index);
+router.get("/:slug", verifyApiKey, topicsController.get);
+module.exports = router;
