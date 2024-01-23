80048ecc758e0b2fc4a01d4e356c60856fc65b16
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-23 15:27:59 +0700

    slicing form login

--- shetalk/src/App.jsx
+++ shetalk/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path='/' element={<LandingPage />} />
             <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
           </Routes>
         </BrowserRouter>
       </div>
