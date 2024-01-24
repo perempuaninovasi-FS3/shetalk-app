c6376729e8db4d8efee637711842bf858ccdc773
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-24 18:09:12 +0700

    slicing profile ahli

--- shetalk/src/App.jsx
+++ shetalk/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import DashboardAhli from './pages/DashboardAhli';
+import ProfileAhli from './pages/ProfileAhli';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/dashboard-ahli' element={<DashboardAhli />} />
+            <Route path='/profile-ahli' element={<ProfileAhli />} />
           </Routes>
         </BrowserRouter>
       </div>
