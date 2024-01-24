ed41ab86d92a8055e94539757864dffd88e9a36d
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-24 17:00:36 +0700

    slicing dashboard ahli

--- shetalk/src/App.jsx
+++ shetalk/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
+import DashboardAhli from './pages/DashboardAhli';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path='/' element={<LandingPage />} />
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/dashboard-ahli' element={<DashboardAhli />} />
           </Routes>
         </BrowserRouter>
       </div>
