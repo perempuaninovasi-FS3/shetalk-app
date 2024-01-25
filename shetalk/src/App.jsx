b0617cf619770d6c0878a55a7d1344572f1021e7
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-25 17:33:20 +0700

    update

--- shetalk/src/App.jsx
+++ shetalk/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <>
       <div>
-        <Dashboard />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
