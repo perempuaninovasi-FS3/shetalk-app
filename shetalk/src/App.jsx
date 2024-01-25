765e32b14558f78855a6b34ee1f2b5a4f5a8de65
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-25 20:55:04 +0700

    fetch update from branch front-end

--- shetalk/src/App.jsx
+++ shetalk/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
+import DashboardAhli from './pages/DashboardAhli';
+import ProfileAhli from './pages/ProfileAhli';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
             <Route path='/' element={<LandingPage />} />
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/dashboard-ahli' element={<DashboardAhli />} />
+            <Route path='/profile-ahli' element={<ProfileAhli />} />
           </Routes>
         </BrowserRouter>
       </div>
