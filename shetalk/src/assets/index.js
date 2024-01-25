ffd1ecde49c840a9face3a319a79eb5c4209c385
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-25 16:38:52 +0700

    make new branch from front-end

--- shetalk/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-import dummyAvatar from './Avatar/dummy-avatar.jpg'
-import IconBeranda from './Icons/icon-beranda.svg'
-import IconJawabPertanyaan from './Icons/icon-jawabPertanyaan.svg'
-import IconTersimpan from './Icons/save-icon.svg'
-export{dummyAvatar, IconBeranda, IconJawabPertanyaan, IconTersimpan}
