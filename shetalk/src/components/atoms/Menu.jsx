1d72bebe62b275fa003bf6a42905864c1062b21d
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-30 15:29:11 +0700

    update page detailPost & menu sidebar

--- shetalk/src/components/atoms/Menu.jsx
+++ shetalk/src/components/atoms/Menu.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Menu = ({ img, title }) => {
   return (
     <div className="custome-color d-flex mb-3 mt-2 gap-2 align-items-center  ">
-      <img src={img} alt="icon-menu" className="d-none d-md-block"/>
+      <img src={img} alt="icon-menu" className="d-none d-md-block" />
       <a href="#" className="d-flex text-decoration-none text-body active align-items-center  " >
         {title}
       </a>
--- shetalk/src/components/molecules/Sidebar.jsx
+++ shetalk/src/components/molecules/Sidebar.jsx
@@ -5,6 +5,7 @@ import { IconBeranda, IconJawabPertanyaan, IconTersimpan } from '../../assets';
 import Topics from '../atoms/Topics';
 import SelectTopics from './SelectTopik';
 import NavMobile from '../atoms/NavMobile';
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
   return (
@@ -17,19 +18,19 @@ const SideBar = () => {
           <SelectTopics />
         </div>
       </div>
- <div className='mt-2 '>
-  <NavMobile />
- </div>
+      <div className='mt-2 '>
+        <NavMobile />
+      </div>
 
- {/* untuk layar desktop */}
+      {/* untuk layar desktop */}
       <div className="d-flex flex-md-column  d-none d-md-block  ">
-        <Menu img={IconBeranda} title="Beranda" />
+        <Link to={'/dashboard'} style={{ textDecoration: 'none' }}><Menu img={IconBeranda} title="Beranda" /></Link>
         <Menu img={IconJawabPertanyaan} title="Jawab Pertanyaan" />
         <Menu img={IconTersimpan} title="Tersimpan" />
       </div>
 
 
-      <div  className='d-none d-md-block mt-5 '>
+      <div className='d-none d-md-block mt-5 '>
         <h2 className="fs-5 mb-4">Topics</h2>
         <Topics fillColor="#FF6565" title="Menstruation" />
         <Topics fillColor="#FC65FF" title="HIV/AIDS" />
