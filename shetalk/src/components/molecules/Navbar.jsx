1a30b3adf403249fa4691fe4c750e859ea917d8f
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-30 15:11:15 +0700

    avatar pick on navbar

--- shetalk/src/components/molecules/Navbar.jsx
+++ shetalk/src/components/molecules/Navbar.jsx
@@ -2,29 +2,25 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Logo from '../atoms/Logo';
 import { useLocation } from 'react-router-dom';
+import { selectSelectedAvatar } from '../../redux/slice/avatarSlice';
 
 const Navbar = () => {
-
-  const user = useSelector((state) => state.user.user);
   const location = useLocation();
-  const selectedProfile = location.state?.selectedProfile;
+  const selectedAvatar = useSelector(selectSelectedAvatar);
 
   return (
-    <div className="d-flex justify-content-center justify-content-md-start align-items-center shadow-sm  px-md-5 py-md-2 px-4 py-2 bg-body-tertiary rounded mb-md-4 mb-2 ">
+    <div className="d-flex justify-content-between align-items-center shadow-sm px-md-5 py-md-2 px-4 py-2 bg-body-tertiary rounded mb-md-4 mb-2">
       <Logo />
-      {/* <a href="#"> */}
-      {selectedProfile && (
-        <div>
-          {/* <span className="me-3">{selectedProfile.username}</span> */}
+      {selectedAvatar && (
+        <div className="ml-auto">
           <img
-            src={selectedProfile.profile}
-            alt="Selected Profile"
+            src={selectedAvatar.avatar_img}
+            alt="Selected Avatar"
             className="rounded-circle"
             style={{ width: '40px', height: '40px', objectFit: 'cover' }}
           />
         </div>
       )}
-      {/* </a> */}
     </div>
   );
 };
