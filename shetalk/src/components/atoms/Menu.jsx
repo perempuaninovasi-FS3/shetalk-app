e5e91e94fbd21319f70ad0a63871ceaeb9d65bca
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-01-30 17:07:51 +0700

    fix error tag a

--- shetalk/src/components/atoms/Menu.jsx
+++ shetalk/src/components/atoms/Menu.jsx
@@ -4,9 +4,9 @@ const Menu = ({ img, title }) => {
   return (
     <div className="custome-color d-flex mb-3 mt-2 gap-2 align-items-center  ">
       <img src={img} alt="icon-menu" className="d-none d-md-block" />
-      <a href="#" className="d-flex text-decoration-none text-body active align-items-center  " >
+      <div className="menu-sidebar d-flex text-decoration-none text-body active align-items-center  " >
         {title}
-      </a>
+      </div>
     </div>
   );
 };
--- shetalk/src/components/atoms/Topics.jsx
+++ shetalk/src/components/atoms/Topics.jsx
@@ -7,9 +7,9 @@ const Topics = ({ fillColor, title }) => {
         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="12" cy="12" r="12" fill={fillColor} />
         </svg>
-        <a href="#" className="d-flex text-decoration-none text-body active align-items-center  ">
+        <div className="menu-sidebar text-decoration-none text-body active align-items-center  ">
           {title}
-        </a>
+        </div>
       </div>
     </>
   );
