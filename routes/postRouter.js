a88f6a6767e367042a0c2130c7b16f1461273ff4
doobeedoobeedam <kwecitra@gmail.com>
2024-02-01 21:37:27 +0700

    make new routes for post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -4,9 +4,20 @@
 
 const express = require("express");
 const router = express.Router();
-
-const verifyApiKeyMiddleware = require("../services/verifyTokenService");
+const verifyApiKeyMiddleware = require("../middlewares/verifyApiKey");
+const verifyUnrequiredToken = require("../services/verifyTokenService"); // authorize/authorization gak wajib
+const postController = require("../controllers/Posts/postController");
+const validator = require("../validators/");
 
 // [api/posts?page=1{for paginate}]
-router.get("/", function (req, res) {});
+router.get("/", verifyApiKeyMiddleware, postController.index);
+router.post(
+  "/",
+  [
+    verifyUnrequiredToken,
+    validator.postValidator("create"),
+    verifyApiKeyMiddleware,
+  ],
+  postController.create_new_post
+);
 module.exports = router;
