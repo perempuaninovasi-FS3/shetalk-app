5d3acc5a97da39020301c2772d67d18b9cf48ab8
doobeedoobeedam <kwecitra@gmail.com>
2024-02-03 17:23:42 +0700

    update some on PostRoute

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -10,9 +10,16 @@ const postController = require("../controllers/Posts/postController");
 const validator = require("../validators/");
 
 // [api/posts?page=1{for paginate}]
-router.get("/", verifyApiKeyMiddleware, postController.index);
+router.get("/posts", verifyApiKeyMiddleware, postController.index);
+
+// [api/post/{for slug}]
+router.get(
+  "/post/:slug",
+  [verifyApiKeyMiddleware, verifyUnrequiredToken],
+  postController.get
+);
 router.post(
-  "/",
+  "/posts",
   [
     verifyUnrequiredToken,
     validator.postValidator("create"),
