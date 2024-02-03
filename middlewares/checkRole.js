adefae750046a6f99097c393c126152d830bc064
doobeedoobeedam <kwecitra@gmail.com>
2024-02-03 17:20:50 +0700

    change's logic for checkRole middleware

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,7 +1,15 @@
 module.exports = function checkRole(allowedRoles) {
   return async (req, res, next) => {
     const userRoles = req.user.role;
-    const hasPermission = allowedRoles.some((role) => userRoles.includes(role));
+
+    // Convert allowedRoles to an array
+    const rolesToCheck = Array.isArray(allowedRoles)
+      ? allowedRoles
+      : allowedRoles.split(",");
+
+    const hasPermission = rolesToCheck.some((role) =>
+      userRoles.includes(role.trim())
+    );
     if (hasPermission) {
       console.log("Akses di dapatkan!");
       next();
