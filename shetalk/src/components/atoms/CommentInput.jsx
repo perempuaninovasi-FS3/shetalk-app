3949e8b1139fbd505b8be399650f646323580b77
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-02-06 18:07:34 +0700

    integrate api comment (create new comment)

--- shetalk/src/components/atoms/CommentInput.jsx
+++ shetalk/src/components/atoms/CommentInput.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from 'react';
 import { getUser, getAvatar } from '../../utils/userUtils';
+import { useDispatch } from 'react-redux';
+import { createComment } from '../../redux/slice/commentSlice';
 
-const CommentInput = () => {
-
+const CommentInput = ({ postId }) => {
   const user = getUser();
   const avatar = getAvatar();
+  const dispatch = useDispatch();
 
-  const [valueComment, setValueComment] = useState('');
+  const [comment, setComment] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {
-    setValueComment(e.target.value);
-  };
 
-  const handleSubmitComment = (e) => {
+  const handleSubmitComment = async (e) => {
     e.preventDefault();
-    if (valueComment.trim() === '') {
-      return false;
-    } else {
-      const newValueComment = { text: valueComment, id: Date.now() };
-      setValueComment('');
+    setLoading(true);
+
+    let comments = { comment }
+
+    try {
+      await dispatch(createComment(comments));
+      setComment('');
+      setLoading(false);
+      alert('Berhasil membuat komentar');
+    } catch (error) {
+      console.error('Gagal membuat komentar:', error);
+      setLoading(false);
+      alert('Gagal membuat komentar');
     }
   };
 
@@ -26,7 +34,9 @@ const CommentInput = () => {
     <div className="d-flex align-items-center gap-3 m-md-5 m-2  bg-white ">
       <img src={user ? user.profile : avatar.avatar_img} alt="Profile" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
       <form onSubmit={handleSubmitComment} className="w-100">
-        <input type="text" placeholder="Tulis balasan" className=" custome-input border-0 ml-2 w-100  border-bottom " value={valueComment} onChange={handleChange} />
+        <input type="text" placeholder="Tulis balasan" className=" custome-input border-0 ml-2 w-100  border-bottom " value={comment} onChange={(e) => setComment(e.target.value)} />
+        <button type="submit">Kirim</button>
+        <div> {loading ? 'Loading...' : ' '}</div>
       </form>
     </div>
   );
--- shetalk/src/redux/slice/commentSlice.js
+++ shetalk/src/redux/slice/commentSlice.js
@@ -1,19 +1,45 @@
-// commentSlice.js
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 
-export const fetchCommentsByPostId = createAsyncThunk('comments/fetchCommentsByPostId', async (postId, thunkAPI) => {
+export const fetchCommentsByPostId = createAsyncThunk('comments/fetchCommentsByPostId', async (_, thunkAPI) => {
     try {
-        const response = await fetch(`${API_URL}/api/comment?post_id=${postId}`, {
+        const post = sessionStorage.getItem('detail-post');
+        const postData = JSON.parse(post);
+        const post_id = postData.id;
+        const response = await fetch(`${API_URL}/api/comment?post_id=${post_id}`, {
             headers: {
                 'API_KEY': API_KEY,
                 'Content-Type': 'application/json',
             },
         });
         const data = await response.json();
-        return data.data;
+        return data.data.comments;
+    } catch (error) {
+        return thunkAPI.rejectWithValue({ error: error.message });
+    }
+});
+
+
+export const createComment = createAsyncThunk('comment/createComment', async (comments, thunkAPI) => {
+    try {
+        const token = localStorage.getItem('token');
+        const post = sessionStorage.getItem('detail-post');
+        const postData = JSON.parse(post);
+        const post_id = postData.id;
+        await axios.post(`${API_URL}/api/comment?post_id=${post_id}`, comments, {
+            headers: {
+                'API_KEY': API_KEY,
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const fetchCommentData = await thunkAPI.dispatch(fetchCommentsByPostId());
+        const data = fetchCommentData.payload;
+        return data;
     } catch (error) {
         return thunkAPI.rejectWithValue({ error: error.message });
     }
@@ -38,7 +64,16 @@ const commentSlice = createSlice({
             .addCase(fetchCommentsByPostId.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.payload.error;
-            });
+            })
+            .addCase(createComment.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.comments = action.payload;
+            })
+            .addCase(createComment.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.payload.error;
+                console.log('gabisa')
+            })
     },
 });
 
