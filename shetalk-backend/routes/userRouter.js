1f17b5bf81fbf183d74bc88156c9ffdbd2a9e700
doobeedoobeedam <kwecitra@gmail.com>
2024-02-10 15:28:58 +0700

    make upload file middleware multer as utils

--- shetalk-backend/routes/userRouter.js
+++ shetalk-backend/routes/userRouter.js
@@ -8,31 +8,7 @@ const path = require("path");
 const userController = require("../controllers/User/userController");
 const verifyAuthMiddleware = require("../middlewares/verifyAuth");
 const validator = require("../validators/");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/profiles");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
-  },
-});
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB limit
-  },
-  fileFilter: (req, file, cb) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(null, false, {
-        status: false,
-        message: "File hanya berupa gambar saja!",
-      });
-    }
-    cb(null, true);
-  },
-});
+const { upload, storage } = require("../utils/uploader.utils");
 
 // [api/user/profile/edit]
 router.put(
@@ -48,7 +24,7 @@ router
   .post(verifyAuthMiddleware, userController.get_user_)
   .get(verifyAuthMiddleware, userController.get_user_);
 
-// [api/user/edit]
+// [api/user/me/edit]
 router.put(
   "/me/edit",
   [validator.userValidator("edit_user"), verifyAuthMiddleware],
---
+++ shetalk-backend/utils/uploader.utils.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/profiles");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10MB limit
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(null, false, {
+        status: false,
+        message: "File hanya berupa gambar saja!",
+      });
+    }
+    cb(null, true);
+  },
+});
+const exported_modules = { upload, storage };
+module.exports = exported_modules;
