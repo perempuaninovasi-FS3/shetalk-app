8a31ecc1ecdd1a01ec3595a30ab90a8e49c6b37d
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-02-10 15:35:26 +0700

    make utils for badge

--- shetalk/src/components/molecules/ModalSertif.jsx
+++ shetalk/src/components/molecules/ModalSertif.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import { getUser } from '../../utils/userUtils';
+import { badgeImage, badgeName } from '../../utils/badgeUtils';
 import bronzeBadge from '../../assets/img/bronzebadge.svg'
 import silverBadge from '../../assets/img/silverbadge.svg'
 import goldBadge from '../../assets/img/goldbadge.svg'
@@ -28,7 +29,6 @@ const ModalSertif = () => {
                     </Modal.Header>
                     <Modal.Body>
                         {totalAnswered < 20 ? (
-                            // Menampilkan informasi jika totalAnswered kurang dari 20
                             <div style={{ color: '#333', fontSize: '16px', lineHeight: '1.5', padding: '2rem' }}>
                                 <p>Hi! Sepertinya anda belum mendapatkan sertifikat, Anda harus berpartisipasi aktif dalam menjawab pertanyaan-pertanyaan user general untuk mendapatkan sertifikat</p>
                                 Ada beberapa tingkatan keaktifan dalam perolehan sertifikat ini:<br />
@@ -40,10 +40,19 @@ const ModalSertif = () => {
                                 <h5 className='mt-5'>Selamat berdiskusi!</h5>
                             </div>
                         ) : (
-                            // Menampilkan konten yang sesuai jika totalAnswered lebih dari atau sama dengan 20
-                            <div>
-                                <p>Selamat ! anda sekarang dalam tingkatan (icon) anda mendapat sertifikat atas keaktifan anda !</p>
-                                <button>Unduh sertifikat disini</button>
+                            <div style={{ color: '#333', fontSize: '16px', lineHeight: '1.5', padding: '2rem' }}>
+                                <p>Selamat ! Anda telah berhasil mendapatkan {badgeName} {badgeImage && <img src={badgeImage} alt="Badge" style={{ width: '50px', height: '50px' }} />}</p>
+                                <p>Anda juga memperoleh sertifikat atas keaktifan anda</p>
+                                <Button className='btn-picks-avatar'
+                                    style={{
+                                        backgroundColor: '#43d7c2',
+                                        border: 'none',
+                                        marginBottom: '14px',
+                                        marginTop: '20px',
+                                        padding: '10px',
+                                        transition: 'transform 0.3s',
+                                        cursor: 'pointer',
+                                    }}>Lihat Sertifikat</Button>
                             </div>
                         )}
                     </Modal.Body>
