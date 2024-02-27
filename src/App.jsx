0d7788efdaf9184a214d3e868e6183ff0978025e
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-02-27 17:07:44 +0700

    add netlify.toml

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import '../src/css/App.css';
-import '../src/css/index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import ProfileAhli from './pages/ProfileAhli';
-import DetailPost from './pages/DetailPost';
-import Sertifikat from './pages/Sertifikat';
+import "../src/css/App.css";
+import "../src/css/index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import ProfileAhli from "./pages/ProfileAhli";
+import DetailPost from "./pages/DetailPost";
+import Sertifikat from "./pages/Sertifikat";
 
 function App() {
   return (
@@ -14,12 +14,12 @@ function App() {
       <div>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<LandingPage />} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/post/:slug' element={<DetailPost />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/profile-ahli' element={<ProfileAhli />} />
-            <Route path='/sertifikat' element={<Sertifikat />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/post/:slug" element={<DetailPost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile-ahli" element={<ProfileAhli />} />
+            <Route path="/sertifikat" element={<Sertifikat />} />
           </Routes>
         </BrowserRouter>
       </div>
