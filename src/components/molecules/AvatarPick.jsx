e73618f4a1e2f22f81c57e0d2eb85ca100c1d5b5
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-02-27 15:19:47 +0700

    update landing page

--- src/components/molecules/AvatarPick.jsx
+++ src/components/molecules/AvatarPick.jsx
@@ -1,120 +1,153 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setSelectedAvatar, selectAvatars, selectSelectedAvatar } from '../../redux/slice/avatarSlice';
-import { useNavigate } from 'react-router-dom';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setSelectedAvatar,
+  selectAvatars,
+  selectSelectedAvatar,
+} from "../../redux/slice/avatarSlice";
+import { useNavigate } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
 
 const AvatarPick = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const avatars = useSelector(selectAvatars);
-    const selectedAvatar = useSelector(selectSelectedAvatar);
+  const avatars = useSelector(selectAvatars);
+  const selectedAvatar = useSelector(selectSelectedAvatar);
 
-    const [loading, setLoading] = useState(false);
-    const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const handleModalShow = () => setShowModal(true);
-    const handleModalClose = () => setShowModal(false);
+  const handleModalShow = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
 
-    const handleProfileSelect = (avatar) => {
-        dispatch(setSelectedAvatar(avatar));
-    };
+  const handleProfileSelect = (avatar) => {
+    dispatch(setSelectedAvatar(avatar));
+  };
 
-    const handleAvatarToUse = () => {
-        if (selectedAvatar) {
-            dispatch(setSelectedAvatar(selectedAvatar));
-            navigate('/dashboard');
-        }
-    };
+  const handleAvatarToUse = () => {
+    if (selectedAvatar) {
+      dispatch(setSelectedAvatar(selectedAvatar));
+      navigate("/dashboard");
+    }
+  };
 
-    return (
-        <div>
-            <Button variant="danger" className='fs-6' size='lg' onClick={handleModalShow}>
-                Gabung
-            </Button>
-            <Modal show={showModal} onHide={handleModalClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Avatar</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center' }}>
-
-                        <div>
-                            <h5 style={{ marginBottom: '20px', marginTop: '10px' }}>Pilih avatar sebelum lanjut ke halaman diskusi</h5>
-                            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                                {
-                                    avatars.map((avatar, id) => (
-                                        <div
-                                            key={id}
-                                            onClick={() => handleProfileSelect(avatar)}
-                                            className={`avatar-container ${selectedAvatar === avatar ? 'selected' : ''}`}
-                                            style={{
-                                                margin: '10px',
-                                                textAlign: 'center',
-                                                border: `2px solid ${avatar.borderColor || '#43d7c2'}`,
-                                                padding: '10px',
-                                                borderRadius: '10px',
-                                                cursor: 'pointer',
-                                            }}
-                                        >
-                                            <img
-                                                src={avatar.avatar_url}
-                                                alt={`Profile ${id + 1}`}
-                                                style={{
-                                                    width: '100px',
-                                                    height: '100px',
-                                                    objectFit: 'cover',
-                                                    borderRadius: '50%',
-                                                    cursor: 'pointer',
-                                                }}
-                                            />
-                                            <p style={{ marginTop: '10px' }}>{avatar.avatar_name}</p>
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                        <div style={{ width: '95%' }}>
-                            <Button
-                                className='btn-picks-avatar'
-                                style={{
-                                    backgroundColor: '#43d7c2',
-                                    border: 'none',
-                                    marginBottom: '14px',
-                                    marginTop: '20px',
-                                    padding: '10px',
-                                    width: '100%',
-                                    transition: 'transform 0.3s',
-                                    cursor: 'pointer',
-                                }}
-                                onClick={handleAvatarToUse}
-                                disabled={loading}
-                            >
-                                {loading ? 'Loading...' : (
-                                    <>
-                                        {selectedAvatar ? (
-                                            <>
-                                                <span>Lanjut sebagai </span>
-                                                <span>{selectedAvatar.avatar_name} </span>
-                                                <img
-                                                    src={selectedAvatar.avatar_url}
-                                                    alt="Selected Profile"
-                                                    style={{ width: '30px', height: '30px', objectFit: 'cover', borderRadius: '50%' }}
-                                                />
-                                            </>
-                                        ) : (
-                                            <span>Pilih dengan 'klik' pada avatar yang kamu sukai</span>
-                                        )}
-                                    </>
-                                )}
-                            </Button>
-                        </div>
-                    </div>
-                </Modal.Body>
-            </Modal>
-        </div>
-    );
+  return (
+    <div>
+      <Button
+        variant="danger"
+        className="fs-6 text-decoration-none btn text-white me-lg-5 btn-lg btn-danger position-relative z-3 border-0"
+        size="lg"
+        onClick={handleModalShow}
+      >
+        Gabung
+      </Button>
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Avatar</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <div>
+              <h5 style={{ marginBottom: "20px", marginTop: "10px" }}>
+                Pilih avatar sebelum lanjut ke halaman diskusi
+              </h5>
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}
+              >
+                {avatars.map((avatar, id) => (
+                  <div
+                    key={id}
+                    onClick={() => handleProfileSelect(avatar)}
+                    className={`avatar-container ${
+                      selectedAvatar === avatar ? "selected" : ""
+                    }`}
+                    style={{
+                      margin: "10px",
+                      textAlign: "center",
+                      border: `2px solid ${avatar.borderColor || "#43d7c2"}`,
+                      padding: "10px",
+                      borderRadius: "10px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <img
+                      src={avatar.avatar_url}
+                      alt={`Profile ${id + 1}`}
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                        cursor: "pointer",
+                      }}
+                    />
+                    <p style={{ marginTop: "10px" }}>{avatar.avatar_name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div style={{ width: "95%" }}>
+              <Button
+                className="btn-picks-avatar"
+                style={{
+                  backgroundColor: "#43d7c2",
+                  border: "none",
+                  marginBottom: "14px",
+                  marginTop: "20px",
+                  padding: "10px",
+                  width: "100%",
+                  transition: "transform 0.3s",
+                  cursor: "pointer",
+                }}
+                onClick={handleAvatarToUse}
+                disabled={loading}
+              >
+                {loading ? (
+                  "Loading..."
+                ) : (
+                  <>
+                    {selectedAvatar ? (
+                      <>
+                        <span>Lanjut sebagai </span>
+                        <span>{selectedAvatar.avatar_name} </span>
+                        <img
+                          src={selectedAvatar.avatar_url}
+                          alt="Selected Profile"
+                          style={{
+                            width: "30px",
+                            height: "30px",
+                            objectFit: "cover",
+                            borderRadius: "50%",
+                          }}
+                        />
+                      </>
+                    ) : (
+                      <span>
+                        Pilih dengan 'klik' pada avatar yang kamu sukai
+                      </span>
+                    )}
+                  </>
+                )}
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 };
 
 export default AvatarPick;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,132 +1,239 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import Logo from '../components/atoms/Logo';
-import tagbrand from '../assets/img/tag-brand.svg';
-import timun from '../assets/img/timun.svg';
-import nawang from '../assets/img/nawang.svg';
-import shetalk from '../assets/img/shetalk.svg';
-import email from '../assets/img/email.svg';
-import instagram from '../assets/img/instagram.svg';
-import tiktok from '../assets/img/tiktok.svg';
-import AvatarPick from '../components/molecules/AvatarPick';
-import { Link } from 'react-router-dom';
-import '../css/LandingPage.css'
+import React, { useState, useEffect } from "react";
+import { Modal, Button } from "react-bootstrap";
+import Logo from "../components/atoms/Logo";
+import tagbrand from "../assets/img/tag-brand.svg";
+import timun from "../assets/img/timun.svg";
+import nawang from "../assets/img/nawang.svg";
+import shetalk from "../assets/img/shetalk.svg";
+import email from "../assets/img/fluent_mail-28-filled.svg";
+import instagram from "../assets/img/ri_instagram-fill.svg";
+import tiktok from "../assets/img/tiktok.svg";
+import AvatarPick from "../components/molecules/AvatarPick";
+import { Link } from "react-router-dom";
+import "../css/LandingPage.css";
+import stiker01 from "../assets/img/sticker01.svg";
+import shetalkDb from "../assets/img/shetalk-db.png";
+import peopleava from "../assets/img/People-ava.svg";
+import group108 from "../assets/img/Group108.svg";
 
 function LandingPage() {
+  useEffect(() => {
+    // IntersectionObserver for elements with class .hiddenX
+    const observerX = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
 
-    return (
-        <>
+    // Observe elements with class .hiddenX
+    const hiddenElementsX = document.querySelectorAll(".hiddenX");
+    hiddenElementsX.forEach((el) => observerX.observe(el));
 
-            {/*Navbar*/}
-            <div className="d-flex shadow-sm  px-md-5 py-md-4 px-4 py-2 bg-body-tertiary rounded">
-                <Logo />
+    // IntersectionObserver for elements with class .hiddenY
+    const observerY = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    // Observe elements with class .hiddenY
+    const hiddenElementsY = document.querySelectorAll(".hiddenY");
+    hiddenElementsY.forEach((el) => observerY.observe(el));
+
+    // Cleanup function
+    return () => {
+      hiddenElementsX.forEach((el) => observerX.unobserve(el));
+      hiddenElementsY.forEach((el) => observerY.unobserve(el));
+    };
+  }, []);
+  return (
+    <>
+      {/* <!-- navbar --> */}
+      <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src={shetalk}
+              alt="Logo"
+              width="auto"
+              height="24"
+              className="d-inline-block align-text-top px-lg-4"
+            />
+          </a>
+        </div>
+      </nav>
+      {/* <!-- start section 1 --> */}
+      <div className="container-fluid costume-container ">
+        <img
+          src={stiker01}
+          alt=""
+          className="position-absolute img-fluid costume-img"
+        />
+        <div className="container ">
+          <div className="row ">
+            <div className="col-12 text-center mt-5 ">
+              <h1 className="costume-text fw-bold hiddenY">
+                Selamat datang di SheTalk!
+              </h1>
             </div>
-
-            {/*Part 1*/}
-            <div className="container-fluid">
-                <div className='row'>
-                    <div className='col align-self-center'>
-                        <figure className="text-center">
-                            <blockquote className="blockquote" data-aos="zoom-in-up">
-                                <h1 className="text1">Selamat datang di SheTalk</h1>
-                                <p className="text2">Platform yang didedikasikan untuk menyediakan</p>
-                                <p className="text2">ruang diskusi terpercaya bagi perempuan untuk curhat seputar</p>
-                                <p className="text3">kesehatan reproduksi dan seksual</p>
-                            </blockquote>
-                        </figure>
-                    </div>
-                </div>
-
-                {/*Part 2 - Tag brand*/}
-                <div className='row justify-content-center'>
-                    <img className='img-fluid' src={tagbrand} style={{ width: '60rem', height: '60rem' }} data-aos="fade-left" />
-                </div>
-
-                {/*Promo*/}
-                <figure className="text-center">
-                    <blockquote className="blockquote">
-                        <h1 className="text4">Mengapa SheTalk?</h1>
-                    </blockquote>
-                </figure>
-
-                <div className="row align-items-start text-center " style={{ margin: '1rem', padding: '1rem' }}>
-                    <div className="col-sm border rounded" data-aos="fade-left">
-                        <h5 className="text5">Anonim dan Aman</h5>
-                        <p className="text6">Privasimu adalah prioritas kami. Berdiskusi tanpa perlu khawatir tentang identitas terungkap.</p>
-                    </div>
-                    <div className="col-sm border rounded" data-aos="fade-left">
-                        <h5 className="text5">Diskusi Terbuka dan Beragam</h5>
-                        <p className="text6">Temukan solusi, bagikan pengalaman, dan berdialog secara positif untuk membangun pengetahuan.</p>
-                    </div>
-                    <div className="col-sm border rounded" data-aos="fade-left">
-                        <h5 className="text5">Sertifikat Penghargaan</h5>
-                        <p className="text6">Jadilah volunteer kesehatan yang aktif berinteraksi dan membangun dampak positif.</p>
-                    </div>
-                </div>
+          </div>
+          <div className="row">
+            <div className="col-12 text-center text-white">
+              <p>
+                Platform yang didedikasikan untuk menyediakan ruang aman <br />{" "}
+                dan terpercaya bagi perempuan untuk diskusi seputar <br />{" "}
+                <span className="costume-text">
+                  {" "}
+                  kesehatan reproduksi dan seksual{" "}
+                </span>
+              </p>
             </div>
-
-            <div className="row justify-content-center align-items-center">
-                <div className="col-md">
-                    <div className="card-img text-center" >
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '200px', width: '200px', borderRadius: '50%', padding: '16px' }} src="https://production.listennotes.com/podcasts/riri-cerita-anak/legenda-roro-jonggrang-Dv-rF2UiX87-uAxS5F-J_0n.300x300.jpg" />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '180px', width: '180px', borderRadius: '70%', padding: '16px' }} src={timun} />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '200px', width: '200px', borderRadius: '50%', padding: '16px' }} src={nawang} />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '180px', width: '180px', borderRadius: '50%', padding: '16px' }} src="https://i0.wp.com/catatanbunda.id/wp-content/uploads/2021/06/Sangkuriang-1.jpg?resize=1000%2C562&ssl=1" />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '200px', width: '200px', borderRadius: '50%', padding: '16px' }} src="https://e7.pngegg.com/pngimages/606/242/png-clipart-keong-emas-fairy-tale-child-legend-folklore-ma-am-child-leaf.png" />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '180px', width: '180px', borderRadius: '50%', padding: '16px' }} src="https://production.listennotes.com/podcasts/riri-cerita-anak/legenda-roro-jonggrang-Dv-rF2UiX87-uAxS5F-J_0n.300x300.jpg" />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '200px', width: '200px', borderRadius: '50%', padding: '16px' }} src={nawang} />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '200px', width: '200px', borderRadius: '70%', padding: '16px' }} src={timun} />
-                        <img className="img-fluid" data-aos="zoom-in" style={{ height: '180px', width: '180px', borderRadius: '50%', padding: '16px' }} src="https://e7.pngegg.com/pngimages/606/242/png-clipart-keong-emas-fairy-tale-child-legend-folklore-ma-am-child-leaf.png" />
-                    </div>
-                </div>
-                <div className="col-md">
-                    <div className="card-text text-center">
-                        <p className="fs-5" data-aos="fade-left">Jadilah bagian dari SheTalk dan mulai perjalanan kamu menuju pemahaman yang lebih baik tentang kesehatan reproduksi dan seksual.</p>
-                        <p className="fs-5" data-aos="fade-left">Bergabunglah sekarang untuk diskusi, belajar, dan berbagi pengalaman. Mari kita jaga kesehatan reproduksi dan seksual kita bersama-sama!</p>
-                    </div>
-                </div>
+          </div>
+          <div className="container">
+            <div className="row position-relative mt-5">
+              <div className="col-lg-9 mx-auto col-sm-4 costume-img-header">
+                <img
+                  src={shetalkDb}
+                  alt="shetalk dashbord"
+                  className="rounded-4 img-fluid shadow p-2 hiddenX"
+                />
+              </div>
+              {/* <!-- untuk svg --> */}
             </div>
+          </div>
+        </div>
+      </div>
 
-            <div className="card" data-aos="zoom-in">
-                <div className="row align-content-auto justify-content-center">
-                    <div className="col-md text-center" style={{ padding: '46px' }}>
-                        <h3 className="card-text fs-2" >Curhat Aman Sehat</h3>
-                    </div>
-                    <div className="col-md text-center" style={{ padding: '46px' }}>
-                        <AvatarPick />
-                    </div>
-                </div>
+      {/* <!-- start card --> */}
+      <div className="container text-center card-container">
+        <div className="row">
+          <div className="col-12 my-5 text-center">
+            <h2 className="costume-text fw-bold ">Mengapa SheTalk?</h2>
+            <p className="line"></p>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center gap-4 cards ">
+          <div className="col-lg-3 mt-lg-4 square hiddenX">
+            <div className=" card mx-auto h-100 w-100 ">
+              <div className=" card-body pt-4 ">
+                <h5 className="costume-text  fs-6 fw-bold mb-4">
+                  Anonim dan Aman
+                </h5>
+                <p className="card-text costume-p">
+                  Privasimu adalah prioritas kami. Berdiskusi tanpa perlu
+                  khawatir tentang identitas terungkap.
+                </p>
+              </div>
             </div>
+          </div>
+          <div className="col-lg-3 mt-lg-4 square hiddenX">
+            <div className=" card mx-auto h-100 w-100 ">
+              <div className=" card-body pt-4">
+                <p className=" costume-text  fs-6 fw-bold   ">
+                  Diskusi Terbuka dan Beragam
+                </p>
+                <p className="card-text costume-p">
+                  Temukan solusi, bagikan pengalaman, dan berdialog secara
+                  positif untuk membangun pengetahuan.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 mt-lg-4 square hiddenX">
+            <div className=" card mx-auto h-100 w-100">
+              <div className=" card-body pt-4">
+                <p className="costume-text  fs-6 fw-bold">Simpan Diskusi</p>
+                <p className="card-text costume-p">
+                  Memudahkan untuk mengakses informasi yang Anda butuhkan kapan
+                  pun diperlukan.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- end card --> */}
 
-            <div className='card-footer' style={{ margin: '12px' }}>
-                <div className="row">
-                    <div className="col">
-                        <div className="card-body">
-                            <img className="img-fluid" src={shetalk} />
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card-body">
-                            <img className="img-fluid" src={email} />
-                            <small className="card-text text-muted">shetalkinfo@gmail.com</small>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card-body">
-                            <img className="img-fluid" src={instagram} />
-                            <small className="card-text text-muted">@shetalk</small>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card-body">
-                            <img className="img-fluid" src={tiktok} />
-                            <small className="card-text text-muted">SheTalk</small>
-                        </div>
-                    </div>
-                </div>
+      {/* <!-- start section 2 --> */}
+      <div className="mt-5 mb-5">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-lg-6 mt-5">
+              <img src={peopleava} alt="avatar-profile" className="img-fluid" />
             </div>
-        </>
-    );
+            <div className="col-lg-6 text-center">
+              <p className="fs-5 costume-p-text text-end">
+                Jadilah bagian dari SheTalk dan mulailah perjalanan kamu menuju
+                pemahaman yang lebih baik tentang kesehatan reproduksi dan
+                seksual.
+                <br />
+                <br />
+                Bergabunglah sekarang untuk mendiskusikan, belajar, dan berbagi
+                pengalaman. Mari kita jaga kesehatan reproduksi dan seksual kita
+                bersama-sama!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- end section 2 --> */}
+
+      {/* <!-- start section 3 --> */}
+      <div className="container">
+        <div className="costume-bot-bg d-flex justify-content-center align-items-center position-relative rounded-4">
+          <div className="d-flex flex-column flex-sm-row justify-content-between align-items-center w-100 px-2 px-lg-5 ">
+            <h1 className="text-white mb-5 mb-lg-0 fs-4">
+              Nyaman di Ruang Aman
+            </h1>
+            <div className="text-center text-sm-left">
+              <AvatarPick />
+            </div>
+            <img
+              src={group108}
+              alt=""
+              className="position-absolute z-1 costume-img"
+            />
+          </div>
+        </div>
+      </div>
+      {/* <!-- end section 3 --> */}
+
+      {/* <!-- footer --> */}
+      <div className="mt-5 container-fluid text-center">
+        <div className=" d-lg-flex justify-content-lg-between mx-auto">
+          <img
+            src={shetalk}
+            alt="Logo"
+            width="auto"
+            height="24"
+            className="d-inline-block align-text-top px-lg-4 mb-3"
+          />
+          <p className="costume-p mb-3">shetalksinfo@shetalk.com</p>
+          <div className="mb-3">
+            <a href="tautan-email" className="text-decoration-none">
+              <img src={email} alt="email" />
+            </a>
+            <a
+              href="tautan-instagram"
+              className="text-decoration-none"
+              style={{ paddingLeft: "4px" }}
+            >
+              <img src={instagram} alt="instagram" />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <!-- end --> */}
+    </>
+  );
 }
 
 export default LandingPage;
